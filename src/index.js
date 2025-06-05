f690f6e608ee6b84c8d03551a6a331ef9a2ce6d8
Farid Sikander <fscode4@gmail.com>
2025-06-05 10:42:33 -0400

    Test file removed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ Configure webpack
 first npm install webpack 
 */
 import "./styles.css";
-import { test } from "./test.js"
-console.log(test)
+
 console.log('Working!')
--- src/test.js
+++
@@ -1,1 +0,0 @@
-export const test = "Webpack properly functioning"
